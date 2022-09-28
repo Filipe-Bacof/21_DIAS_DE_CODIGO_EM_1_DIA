d4663c1c1617262af5d211ebfbae8f5ca373de91
Filipe Bacof <filipebacof@gmail.com>
2022-09-28 14:11:13 -0300

    botão do gmail funcionando

--- curriculo-filipe-bacof/script.js
+++ curriculo-filipe-bacof/script.js
@@ -6,3 +6,15 @@ function mostrar(info) {
     }
     document.querySelector(`.${info}`).style.display = "block";
 }
+
+function mostrarEmail () {
+    let texto = document.querySelector('.meu-email-clique');
+    let email = document.querySelector('.meu-email');
+    if (email.style.display == "none") {
+        texto.style.display = "none";
+        email.style.display = "block";
+    } else {
+        texto.style.display = "block";
+        email.style.display = "none";
+    }
+}
